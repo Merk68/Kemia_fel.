08edc3854688f703e8e598ef09eca01998d41cba
Merk68 <marko.mark2001@gmail.com>
2021-10-05 10:15:17 +0200

    Add project files.

---
+++ Kémia/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kémia
+{
+  class Program
+  {
+    List<data> lista = new List<data>();
+    static void Main(string[] args)
+    {
+      int[] tomb = new int[500];
+      MasodikFeladat();
+    }
+
+    private static void MasodikFeladat()
+    {
+      StreamReader olv = new StreamReader("felfedezesek.csv");
+      while (!olv.EndOfStream)
+      {
+
+      }
+      olv.Close();
+    }
+  }
+}
---
+++ Kémia/data.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kémia
+{
+  class data
+  {
+    private string Ev;
+
+    public string ev
+    {
+      get { return Ev; }
+      set { Ev = value; }
+    }
+    private string nev;
+
+    public string Nev
+    {
+      get { return nev; }
+      set { nev = value; }
+    }
+    private string vegyjel;
+
+    public string Vegyjel
+    {
+      get { return vegyjel; }
+      set { vegyjel = value; }
+    }
+    private int rszam;
+
+    public int Rszam
+    {
+      get { return rszam; }
+      set { rszam = value; }
+    }
+    private string tudos;
+
+    public string Tudos
+    {
+      get { return tudos; }
+      set { tudos = value; }
+    }
+
+    public data(string ev, string nev, string vegyjel, int rszam, string tudos)
+    {
+      Ev = ev;
+      this.nev = nev;
+      this.vegyjel = vegyjel;
+      this.rszam = rszam;
+      this.tudos = tudos;
+    }
+  }
+}
