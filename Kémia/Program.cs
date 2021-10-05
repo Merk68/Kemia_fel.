485ba52f6db8688d8c8b2d6136aaa845e33bbc76
Merk68 <marko.mark2001@gmail.com>
2021-10-05 10:22:24 +0200

    Beolvasás(hiányos) + 3.feladat

--- Kémia/Program.cs
+++ Kémia/Program.cs
@@ -9,11 +9,23 @@ namespace Kémia
 {
   class Program
   {
-    List<data> lista = new List<data>();
+    static List<data> lista = new List<data>();
     static void Main(string[] args)
     {
       int[] tomb = new int[500];
       MasodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+    }
+
+    private static void NegyedikFeladat()
+    {
+      
+    }
+
+    private static void HarmadikFeladat()
+    {
+      Console.WriteLine($"3.Feladat: Elemek száma: {lista.Count}");
     }
 
     private static void MasodikFeladat()
@@ -21,7 +33,8 @@ namespace Kémia
       StreamReader olv = new StreamReader("felfedezesek.csv");
       while (!olv.EndOfStream)
       {
-
+        string[] seged = olv.ReadLine().Split(';');
+        lista.Add(new data(int.Parse(seged[0])));
       }
       olv.Close();
     }
