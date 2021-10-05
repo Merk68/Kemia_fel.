cb270fb7a773965cd7a3d5e1bb14391419f6dd22
Merk68 <marko.mark2001@gmail.com>
2021-10-05 23:18:46 +0200

    Hiányos, feladat megoldás.

--- Kémia/Program.cs
+++ Kémia/Program.cs
@@ -16,9 +16,89 @@ namespace Kémia
       MasodikFeladat();
       HarmadikFeladat();
       NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      HetedikFeladat();
+      NyolcadikFeladaz();
     }
 
-    private static void NegyedikFeladat()
+        private static void NyolcadikFeladaz()
+        {
+            Dictionary<string, int> stat = new Dictionary<string, int>();
+            int i = 0;
+            while (lista[i].ev == "Ókor")
+            {
+                i++;
+            }
+            for (int j = 0; j < lista.Count; j++)
+            {
+                if (!stat.ContainsKey(lista[j].ev))
+                {
+                    stat.Add(lista[j].ev, 1);
+                }
+                else
+                {
+                    stat(lista[j].ev)++;
+                }
+                j++;
+            }
+            Console.WriteLine("8.Feladat: Statisztika");
+            foreach (var s in stat)
+            {
+                if (s.Value > 3)
+                {
+                    Console.WriteLine($"{s.Key} : {s.Value} db");
+                }
+            }
+        }
+
+        private static void HetedikFeladat()
+        {
+            int i = 0;
+            
+            while (lista[i].ev == "Ókor")
+            {
+                i++;
+            }
+            int max = Convert.ToInt32(lista[i+1].ev) - Convert.ToInt32(lista.[i].ev);
+            while (i < lista.Count -1)
+            {
+                if (max < Convert.ToInt32(lista[i + 1].ev) - Convert.ToInt32(lista.[i].ev))
+                {
+                    max = Convert.ToInt32(lista[i + 1].ev) - Convert.ToInt32(lista.[i].ev);
+                }
+            }
+            Console.WriteLine($"7.Feladat:{max} év volt!");
+        }
+
+        private static void HatodikFeladat()
+        {
+            int i = 0;
+            while (i < lista.Count && lista[i].Vegyjel.ToLower() != Vegyjel.ToLower())
+            {
+                i++;
+            }
+            if (i < lista.Count)
+            {
+                Console.WriteLine("Keresés:");
+                Console.WriteLine($"Az elem vegyjele: {lista[i].Vegyjel}");
+                Console.WriteLine($"Az elem neve: {lista[i].Nev}");
+                Console.WriteLine($"Rendszáma: {lista[i].Rszam}");
+                Console.WriteLine($"Felfedezés éve: {lista[i].ev}");
+                Console.WriteLine($"Felfedező: {lista[i].Tudos}");
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen elem az adatforrásban.");
+            }
+        }
+
+        private static void OtodikFeladat()
+        {
+            
+        }
+
+        private static void NegyedikFeladat()
     {
       int db = 0;
       foreach (var l in lista)
@@ -41,8 +121,8 @@ namespace Kémia
       StreamReader olv = new StreamReader("felfedezesek.csv");
       while (!olv.EndOfStream)
       {
-        string[] seged = olv.ReadLine().Split(';');
-        lista.Add(new data(int.Parse(seged[0])));
+        string[] a = olv.ReadLine().Split(';');
+        lista.Add(new data(a[0], a[1], a[2], Convert.ToInt32(a[3]), a[4]));
       }
       olv.Close();
     }
